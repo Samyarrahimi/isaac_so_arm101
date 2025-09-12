9ce2a4d78b03e06a4f278f404ecf738087c31904
louislelay <louislelay@pal-robotics.com>
2025-09-12 12:39:14 +0200

    corrects license header duplicate

--- scripts/rsl_rl/cli_args.py
+++ scripts/rsl_rl/cli_args.py
@@ -8,11 +8,6 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
 from __future__ import annotations
 
 import argparse
